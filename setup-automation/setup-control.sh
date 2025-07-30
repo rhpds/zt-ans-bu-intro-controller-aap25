e999f9747815f6ba757da95eae172e2b39ff4bfe
Nuno Martins <nmartins@redhat.com>
2025-07-30 20:21:41 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -820,6 +820,6 @@ tee /tmp/setup.yml << EOL
       tags:
         - check-jt_apache
 
-EOF
+EOL
 
 ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml
