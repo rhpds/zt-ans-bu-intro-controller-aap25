493224b00b5882739ec3a8e2719945463f44a634
Nuno Martins <nmartins@redhat.com>
2025-07-30 20:17:40 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-curl -k  -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt
-update-ca-trust
-rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm
-
-subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}
-# systemctl stop systemd-tmpfiles-setup.service
-# systemctl disable systemd-tmpfiles-setup.service
+systemctl stop systemd-tmpfiles-setup.service
+systemctl disable systemd-tmpfiles-setup.service
 
 
 # Install collection(s)
