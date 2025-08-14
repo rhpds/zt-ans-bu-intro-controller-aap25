38677a72e43cf982514ae3ca5d57f2486e1ca69f
Nuno Martins <nmartins@redhat.com>
2025-08-14 12:14:34 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -7,6 +7,33 @@ systemctl disable systemd-tmpfiles-setup.service
 # Install collection(s)
 # ansible-galaxy collection install community.general
 
+nmcli connection add type ethernet con-name enp2s0 ifname enp2s0 ipv4.addresses 192.168.1.10/24 ipv4.method manual connection.autoconnect yes
+nmcli connection up enp2s0
+echo "192.168.1.10 control.lab control controller" >> /etc/hosts
+
+echo "%rhel ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/rhel_sudoers
+chmod 440 /etc/sudoers.d/rhel_sudoers
+echo "Checking SSH keys for rhel user..."
+
+RHEL_SSH_DIR="/home/rhel/.ssh"
+RHEL_PRIVATE_KEY="$RHEL_SSH_DIR/id_rsa"
+RHEL_PUBLIC_KEY="$RHEL_SSH_DIR/id_rsa.pub"
+
+if [ -f "$RHEL_PRIVATE_KEY" ]; then
+    echo "SSH key already exists for rhel user: $RHEL_PRIVATE_KEY"
+else
+    echo "Creating SSH key for rhel user..."
+    sudo -u rhel mkdir -p /home/rhel/.ssh
+    sudo -u rhel chmod 700 /home/rhel/.ssh
+    sudo -u rhel ssh-keygen -t rsa -b 4096 -C "rhel@$(hostname)" -f /home/rhel/.ssh/id_rsa -N "" -q
+    sudo -u rhel chmod 600 /home/rhel/.ssh/id_rsa*
+    
+    if [ -f "$RHEL_PRIVATE_KEY" ]; then
+        echo "SSH key created successfully for rhel user"
+    else
+        echo "Error: Failed to create SSH key for rhel user"
+    fi
+fi
 
 # # ## setup rhel user
 touch /etc/sudoers.d/rhel_sudoers
