13684418cd50df3e95e47ae448ccda3af3eea754
Nuno Martins <nmartins@redhat.com>
2025-09-01 12:55:32 +0200

    Update solve-control.sh

--- runtime-automation/module-08/solve-control.sh
+++ runtime-automation/module-08/solve-control.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
 echo "Solved module called module-02" >> /tmp/progress.log
+
+## solve-job_template2
+
+## run tasks from setup plabyook / add node3 to inventory
+su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags solve-node3'
+
+## run tasks from setup plabyook / Create Extended services Job Template
+su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags solve-job_template2'
+
+## run tasks from setup plabyook /Create set motd Job Template
+su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags solve-job_template3'
