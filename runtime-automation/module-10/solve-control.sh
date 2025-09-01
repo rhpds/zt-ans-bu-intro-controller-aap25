c7838e074fd60bf625b75190c2d3fac5fef3431f
Nuno Martins <nmartins@redhat.com>
2025-09-01 12:48:54 +0200

    Update solve-control.sh

--- runtime-automation/module-10/solve-control.sh
+++ runtime-automation/module-10/solve-control.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
 echo "Solved module called module-02" >> /tmp/progress.log
+
+## solve-jt_survey
+
+
+cd /home/rhel/ansible-files
+
+## download json
+/usr/bin/curl https://raw.githubusercontent.com/leogallego/instruqt-wyfp/main/files/apache_survey.json -o /home/rhel/ansible-files/apache_survey.json
+
+
+mkdir /tmp/files
+cp /home/rhel/ansible-files/apache_survey.json /tmp/files
+
+
+## run tasks from setup plabyook
+su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags solve-jt_survey --mode stdout'
