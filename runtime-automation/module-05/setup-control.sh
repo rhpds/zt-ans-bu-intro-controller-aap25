081ef469525403b8f3ed03ce6ff3bc6b278d2d29
Nuno Martins <nmartins@redhat.com>
2025-09-01 13:00:46 +0200

    Update setup-control.sh

--- runtime-automation/module-05/setup-control.sh
+++ runtime-automation/module-05/setup-control.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
-su --login rhel -c '/home/rhel/.local/bin/ansible-navigator images'
+## run tasks from setup plabyook
+su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags solve-credentials'
