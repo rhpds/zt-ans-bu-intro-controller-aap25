5949290176d7b568cccec8c05ab52405a4144721
Nuno Martins <nmartins@redhat.com>
2025-09-01 12:53:54 +0200

    Update solve-control.sh

--- runtime-automation/module-09/solve-control.sh
+++ runtime-automation/module-09/solve-control.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 echo "Solved module called module-02" >> /tmp/progress.log
+
+su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags solve-workflow'
