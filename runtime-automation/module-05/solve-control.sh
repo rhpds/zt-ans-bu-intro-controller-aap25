74dfeaa6bf13e2bb224c542c960be98e279db3a1
Nuno Martins <nmartins@redhat.com>
2025-09-01 13:00:21 +0200

    Update solve-control.sh

--- runtime-automation/module-05/solve-control.sh
+++ runtime-automation/module-05/solve-control.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 echo "Solved module called module-02" >> /tmp/progress.log
+
+## "solve-credentials"
+
+## run tasks from setup plabyook
+su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags solve-credentials'
