c1893902c6292911c25cc898258f2f9ee9924eb3
Nuno Martins <nmartins@redhat.com>
2025-09-01 12:57:02 +0200

    Update solve-control.sh

--- runtime-automation/module-07/solve-control.sh
+++ runtime-automation/module-07/solve-control.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 echo "Solved module called module-02" >> /tmp/progress.log
+
+## solve-project2
+
+## run tasks from setup plabyook
+su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags  solve-project2'
