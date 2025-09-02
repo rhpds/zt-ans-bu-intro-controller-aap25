3c5ee21300945e6fd1f2e3b5f387d682cf3b6fd7
Nuno Martins <nmartins@redhat.com>
2025-09-02 12:00:24 +0200

    Update solve-control.sh

--- runtime-automation/module-03/solve-control.sh
+++ runtime-automation/module-03/solve-control.sh
@@ -7,4 +7,4 @@ echo "Solved module called module-02" >> /tmp/progress.log
 #su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags solve-inventory-all'
 
 
-ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml --tags solve-inventory-all
+su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml --tags solve-inventory-all
