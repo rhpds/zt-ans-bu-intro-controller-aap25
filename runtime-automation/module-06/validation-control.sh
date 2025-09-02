9f9b5a764a3b12f8db1c3b30acd6054f088e0e95
Nuno Martins <nmartins@redhat.com>
2025-09-02 13:16:41 +0200

    Update validation-control.sh

--- runtime-automation/module-06/validation-control.sh
+++ runtime-automation/module-06/validation-control.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 echo "Validated module called module-02" >> /tmp/progress.log
-
 set -e
-
 INVENTORY="Lab-Inventory"
 PROJECT="Apache playbooks"
 PROJECT2="Additional playbooks"
@@ -12,16 +10,12 @@ TEMPLATE3="Extended services"
 WORKFLOW="Your first workflow"
 HOSTS=(node1 node2)
 GROUP="web"
-
-
 #Ansible settings
-export ANSIBLE_STDOUT_CALLBACK="community.general.yaml"
-
+export ANSIBLE_STDOUT_CALLBACK="yaml"
 ## Run --tags check-job_template
-CMD="su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags=check-job_template --mode stdout'"
-
+CMD="ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml --tags check-job_template"
 # Check $TEMPLATE_APACHE exists.
 if ! eval "$CMD"; then
-  echo "FAIL: "${TEMPLATE_APACHE}" job template not found or something else is wrong. Remember it's case-sensitive! Please try again."
+  echo "FAIL: ${TEMPLATE_APACHE} job template not found or something else is wrong. Remember it's case-sensitive! Please try again."
   exit 1
 fi
