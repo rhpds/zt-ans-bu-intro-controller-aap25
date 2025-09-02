c6ba1281721f753faa2dc8fae3e1f6e2417b5b4e
Nuno Martins <nmartins@redhat.com>
2025-09-02 13:21:50 +0200

    Update validation-control.sh

--- runtime-automation/module-07/validation-control.sh
+++ runtime-automation/module-07/validation-control.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 echo "Validated module called module-02" >> /tmp/progress.log
-
 set -e
-
-
 INVENTORY="Lab-Inventory"
 PROJECT="Apache playbooks"
 PROJECT2="Additional playbooks"
@@ -13,14 +10,10 @@ TEMPLATE3="Extended services"
 WORKFLOW="Your first workflow"
 HOSTS=(node1 node2)
 GROUP="web"
-
 #Ansible settings
-export ANSIBLE_STDOUT_CALLBACK="community.general.yaml"
-
-
-## Run --tags check-project 2
-CMD="su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags check-project2 --mode stdout'"
-
+export ANSIBLE_STDOUT_CALLBACK="yaml"
+## Run --tags check-project2
+CMD="ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml --tags check-project2"
 # Check $PROJECT2 exists.
 if ! eval "$CMD"; then
   echo "FAIL: ${PROJECT2} project not found or something else is wrong. Remember it's case-sensitive! Please try again."
