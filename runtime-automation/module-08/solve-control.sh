f617b319c9f701973fde43dec4d751a848d9a263
Nuno Martins <nmartins@redhat.com>
2025-09-02 13:25:43 +0200

    Update solve-control.sh

--- runtime-automation/module-08/solve-control.sh
+++ runtime-automation/module-08/solve-control.sh
@@ -4,10 +4,10 @@ echo "Solved module called module-02" >> /tmp/progress.log
 ## solve-job_template2
 
 ## run tasks from setup plabyook / add node3 to inventory
-su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags solve-node3'
+ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml --tags solve-node3
 
 ## run tasks from setup plabyook / Create Extended services Job Template
-su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags solve-job_template2'
+ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml --tags solve-job_template2
 
 ## run tasks from setup plabyook /Create set motd Job Template
-su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags solve-job_template3'
+ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml --tags solve-job_template3
