e8978f60309c0b043fa2408db9f69ae925ed6260
Nuno Martins <nmartins@redhat.com>
2025-09-02 13:19:04 +0200

    Update solve-control.sh

--- runtime-automation/module-07/solve-control.sh
+++ runtime-automation/module-07/solve-control.sh
@@ -4,4 +4,6 @@ echo "Solved module called module-02" >> /tmp/progress.log
 ## solve-project2
 
 ## run tasks from setup plabyook
-su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags  solve-project2'
+#su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags  solve-project2'
+
+ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml --tags  solve-project2
