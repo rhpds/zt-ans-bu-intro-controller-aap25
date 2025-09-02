7decf1bb1cdd0d1b65eca7898702984c2e494c2b
Nuno Martins <nmartins@redhat.com>
2025-09-02 11:36:55 +0200

    Update validation-control.sh

--- runtime-automation/module-03/validation-control.sh
+++ runtime-automation/module-03/validation-control.sh
@@ -12,12 +12,12 @@ export ANSIBLE_STDOUT_CALLBACK="community.general.yaml"
 
 ## Check all
 
-CMDALL="su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags check-inventory-all'"
+CMDALL="ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml --tags check-inventory-all'"
 
 
 ## Check $INVENTORY exists.
 
-CMDINV="su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags check-inventory --mode stdout'"
+CMDINV="ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml --tags check-inventory --mode stdout'"
 
 if ! eval "$CMDINV"; then
   echo "FAIL: ${INVENTORY} inventory not found. Remember it's case-sensitive! Please try again."
@@ -25,23 +25,23 @@ if ! eval "$CMDINV"; then
 fi
 
 
-## Check hosts are in $INVENTORY
+# ## Check hosts are in $INVENTORY
 
-CMDHOSTS="su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags check-inv-hosts --mode stdout'"
+# CMDHOSTS="su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags check-inv-hosts --mode stdout'"
 
-for host in "${HOSTS[@]}"; do
-  if ! eval "$CMDHOSTS"; then
-  echo "FAIL: node1 or node2 are missing from ${INVENTORY} or there is a duplicate host in another inventory. If so, please remove the duplicate ${host} host and check again. Remember it's case-sensititve!"
-  exit 1
-  fi
-done
+# for host in "${HOSTS[@]}"; do
+#   if ! eval "$CMDHOSTS"; then
+#   echo "FAIL: node1 or node2 are missing from ${INVENTORY} or there is a duplicate host in another inventory. If so, please remove the duplicate ${host} host and check again. Remember it's case-sensititve!"
+#   exit 1
+#   fi
+# done
 
-## Check $GROUP is in $INVENTORY
+# ## Check $GROUP is in $INVENTORY
 
-CMDGROUP="su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags check-inv-group --mode stdout'"
+# CMDGROUP="su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags check-inv-group --mode stdout'"
 
-if ! eval "$CMDGROUP"; then
-    echo "FAIL: ${GROUP} group is missing, node1 and node2 are not in the group, or there is a duplicate ${GROUP} group in another inventory. If so, please remove the duplicate ${GROUP} group and check again. Remember it's case-sensititve!"
-    echo "Remember it's case-sensititve! Please try again."
-    exit 1
-fi
+# if ! eval "$CMDGROUP"; then
+#     echo "FAIL: ${GROUP} group is missing, node1 and node2 are not in the group, or there is a duplicate ${GROUP} group in another inventory. If so, please remove the duplicate ${GROUP} group and check again. Remember it's case-sensititve!"
+#     echo "Remember it's case-sensititve! Please try again."
+#     exit 1
+# fi
