01dd65f327c5743e31f92fb105d12aad0352ba21
Nuno Martins <nmartins@redhat.com>
2025-09-02 13:27:04 +0200

    Update solve-control.sh

--- runtime-automation/module-10/solve-control.sh
+++ runtime-automation/module-10/solve-control.sh
@@ -15,4 +15,4 @@ cp /home/rhel/ansible-files/apache_survey.json /tmp/files
 
 
 ## run tasks from setup plabyook
-su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags solve-jt_survey --mode stdout'
+ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml --tags solve-jt_survey --mode stdout'
