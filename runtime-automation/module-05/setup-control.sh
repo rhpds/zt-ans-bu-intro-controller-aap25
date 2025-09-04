425f209aaf4a2ba3174be1ade9cf4e66e0bc11bd
Nuno Martins <nmartins@redhat.com>
2025-09-04 12:28:24 +0200

    Update setup-control.sh

--- runtime-automation/module-05/setup-control.sh
+++ runtime-automation/module-05/setup-control.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ## run tasks from setup plabyook
-su --login rhel -c '/home/rhel/.local/bin/ansible-navigator run /tmp/controller-101-setup.yml --tags solve-credentials'
+ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml --tags solve-credentials
